1ac807142ef3fde09bf2670aba41f336154cb68b
Myroslava Tarvcha <tarcham1993@gmail.com>
2015-04-26 20:56:10 +0300

    Add User, UserProfile, Friends to DataContracts

---
+++ Common/Common.DataContracts/User.cs
@@ -0,0 +1,89 @@
+﻿// // <copyright company="Tarcha & Ivchenko Company">
+// //      Copyright (c) 2015, All Right Reserved
+// // </copyright>
+// // <author>Ivan Ivchenko</author>
+// // <author>Myroslava Tarcha</author>
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace Common.DataContracts
+{
+    /// <summary>
+    /// Represents a simple chat user.
+    /// </summary>
+    [DataContract]
+    public class User
+    {
+        private readonly Friends _friends;
+        private UserProfile _profile;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class.
+        /// This constructor we need for DB working.
+        /// </summary>
+        public User()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class.
+        /// </summary>
+        /// <param name="profile">The personal users info.</param>
+        public User(UserProfile profile)
+        {
+            _profile = profile;
+            _friends = new Friends();
+            Id = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Gets users id.
+        /// </summary>
+        [Key]
+        public Guid Id { get; private set; }
+
+        /// <summary>
+        /// Gets or sets a personal info about user.
+        /// </summary>
+        /// <value>The users profile.</value>
+        [DataMember]
+        public UserProfile Profile
+        {
+            get
+            {
+                return _profile;
+            }
+
+            set
+            {
+                _profile = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the collection of users contacts.
+        /// </summary>
+        /// <value>The users friends.</value>
+        [DataMember]
+        [NotMapped]
+        public Friends Friends
+        {
+            get
+            {
+                return _friends;
+            }
+        }
+
+        /// <summary>
+        /// Adds a new contact to users friends.
+        /// </summary>
+        /// <param name="friend">Other user.</param>
+        public void AddFriend(User friend)
+        {
+            _friends.Add(friend);
+        }
+    }
+}
---
+++ Common/Common.DataContracts/UserProfile.cs
@@ -0,0 +1,40 @@
+﻿// // <copyright company="Tarcha & Ivchenko Company">
+// //      Copyright (c) 2015, All Right Reserved
+// // </copyright>
+// // <author>Ivan Ivchenko</author>
+// // <author>Myroslava Tarcha</author>
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace Common.DataContracts
+{
+    /// <summary>
+    /// Represents a personal info about user.
+    /// </summary>
+    [DataContract]
+    public class UserProfile
+    {
+        /// <summary>
+        /// Gets or sets an account name of user.
+        /// </summary>
+        /// <value>The name of the user.</value>
+        [DataMember]
+        public string AccountName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hash of entered password.
+        /// </summary>
+        /// <value>The users password hash.</value>
+        [DataMember]
+        public int PasswordHash { get; set; }
+
+        /// <summary>
+        /// Gets or sets a users age.
+        /// </summary>
+        /// <value>The users age.</value>
+        [DataMember]
+        public int Age { get; set; }
+    }
+}
