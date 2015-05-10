f19010ebe8111a3efcc49e621cf7f08efb527448
Myroslava Tarcha <miroslava.tarcha@softheme.com>
2015-05-10 21:12:13 +0300

    Implement BehaviorExtensionElement
    Just for service configuration file
    
    TK: 420507

---
+++ Server/Server.Implementation/UnityServiceBehaviorExtensionElement.cs
@@ -0,0 +1,34 @@
+﻿// <copyright company="Tarcha & Ivchenko Company">
+//      Copyright (c) 2015, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <author>Myroslava Tarcha</author>
+
+using System;
+using System.ServiceModel.Configuration;
+using Common.Implementation.IocInWCF;
+
+namespace Server.Implementation
+{
+    /// <summary>
+    /// Configuration element.
+    /// </summary>
+    public class UnityServiceBehaviorExtensionElement : BehaviorExtensionElement
+    {
+        /// <summary>
+        /// Gets type of UnityServiceBehavior.
+        /// </summary>
+        public override Type BehaviorType
+        {
+            get { return typeof(UnityServiceBehavior); }
+        }
+
+        /// <summary>
+        /// Creates and returns UnityServiceBehavior.
+        /// </summary>
+        protected override object CreateBehavior()
+        {
+            return new UnityServiceBehavior(UnityContainerHolder.UnityContainer);
+        }
+    }
+}
